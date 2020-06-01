d429f2a949bee6f7d301f120fd4cd0f3b0eb384d
AliabbasMerchant <merchantaliabbas@gmail.com>
2020-06-01 08:34:41 +0530

    Function to Handle URL

--- getFolderFromRepo.py
+++ getFolderFromRepo.py
@@ -28,20 +28,19 @@ if len(TOKEN) == 0:
 
 
 def decouple_url(url: str) -> dict:
-    
     tmp_url = url
     tmp_url = tmp_url.strip('/') + str('/')
     tmp_url = tmp_url[tmp_url.index('github.com'):]
     tokens = tmp_url.split('/')
-
-    if tokens[3]!='':
+    if tokens[3] != '':
         branch = tokens[4]
-        path = url[url.index(branch)+len(branch)+1:]
+        path = '/'.join(tokens[5:])
     else:
         branch = ''
         path = '/'
 
     return {'owner': tokens[1], 'repo': tokens[2], 'path': path, 'last_element': get_last_element(url), 'branch': branch}
+
 
 def log(*args, is_debug: bool = False, **kwargs) -> None:
     if is_debug:
@@ -119,42 +118,29 @@ def show_help():
 
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) == 2:
-        if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-            show_help()
-            exit()
+    def handle_url(url):
         try:
-            url_details = decouple_url(sys.argv[1])
+            url_details = decouple_url(url)
 
             log(f'Owner: {url_details["owner"]}', is_debug=True)
             log(f'Repo: {url_details["repo"]}', is_debug=True)
             log(f'Branch: {url_details["branch"]}', is_debug=True)
             log(f'Path: {url_details["path"]}', is_debug=True)
-
         except ValueError:
-            print('Please enter a valid GitHub Folder/File URL')
+            print(f'{url} is not a valid GitHub Folder/File URL')
             show_help()
             exit(-1)
-
         get_item_from_repo(**url_details)
 
+    import sys
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+            show_help()
+            exit()
+        else:
+            handle_url(sys.argv[1])
     elif len(sys.argv) > 2:
         for url in sys.argv[1:]:
-            try:
-                url_details = decouple_url(url)
-
-                log(f'Owner: {url_details["owner"]}', is_debug=True)
-                log(f'Repo: {url_details["repo"]}', is_debug=True)
-                log(f'Branch: {url_details["branch"]}', is_debug=True)
-                log(f'Path: {url_details["path"]}', is_debug=True)
-
-            except ValueError:
-                print('Please enter a valid GitHub Folder/File URL')
-                show_help()
-                exit(-1)
-
-            get_item_from_repo(**url_details)
-
+            handle_url(url)
     else:
         show_help()
